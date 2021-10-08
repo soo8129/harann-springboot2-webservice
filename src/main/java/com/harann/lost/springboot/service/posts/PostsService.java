24242a136ab9274296e7da93e4611c6c82bd4d52
soo8129 <soo8129@gmail.com>
2021-10-08 17:29:39 +0900

    api test

---
+++ src/main/java/com/harann/lost/springboot/service/posts/PostsService.java
@@ -0,0 +1,19 @@
+package com.harann.lost.springboot.service.posts;
+
+import com.harann.lost.springboot.domain.posts.PostsRepository;
+import com.harann.lost.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
