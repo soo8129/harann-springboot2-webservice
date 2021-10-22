13224027340306286d917e0dd34fb62258a6f9d1
soo8129 <soo8129@gmail.com>
2021-10-22 18:38:20 +0900

    게시글 수정

--- src/main/java/com/harann/lost/springboot/web/IndexController.java
+++ src/main/java/com/harann/lost/springboot/web/IndexController.java
@@ -1,10 +1,12 @@
 package com.harann.lost.springboot.web;
 
 import com.harann.lost.springboot.service.posts.PostsService;
+import com.harann.lost.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -16,5 +18,18 @@ public class IndexController {
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
         return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
     }
 }
